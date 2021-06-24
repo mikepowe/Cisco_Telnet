6804a8f2eec0b86a0455ff9de18e3a82aae1dcd5
Mike Powell <mikepowe@cisco.com>
2021-06-24 15:38:38 -0500

    updated file with ssh details

--- ssh_with_input_loop.py
+++ ssh_with_input_loop.py
@@ -27,7 +27,7 @@ COMMANDS.TXT. COMMANDS SHOULD FOLLOW STANDARD CISCO SYNTAX
 
 def open_conn(ip):
     try:
-        #Define telnet parameters
+        #Define ssh parameters
         username = 'pomi9003'
         password = 'Dnielsen$954'
         port = 22
@@ -36,77 +36,14 @@ def open_conn(ip):
 
         #Logging into device
         connection = paramiko.SSHClient()
-        connection.connect(ip, username=username, password=password)
+        connection.connect(hostname=ip, username=username, password=password)
 #        print("connecting to " + ip + "\n")
 
-        #Waiting for the username prompt and sending the username
-        output = connection.read_until("Username:", reading_timeout)
-        connection.write(username + "\n")
-        time.sleep(2)
+        stdin,stdout,stderr=conn.exec_command('show run | include 10.7.79.250')
+        host_output = stdout.readlines()
 
-        #Waiting for the password prompt and sending the password
-        output = connection.read_until("Password:", reading_timeout)
-        connection.write(password + "\n")
-        time.sleep(2)
-
-
-        #Setting terminal length for entire output - no pagination
-        connection.write("terminal length 0\n")
-        time.sleep(1)
-
-
-        #Enter Enable Mode (Priv lvl 15)
-        connection.write("enable\n")
-        time.sleep(2)
-
-        #Waiting for the password prompt and sending the password
-        output = connection.read_until("Password:", reading_timeout)
-        connection.write(password + "\n")
-        time.sleep(2)
-
-        #Entering global config mode
-        connection.write("\n")
-        connection.write("configure terminal\n")
-        time.sleep(2)
-
-        #Open user selected file for reading
-        selected_cmd_file = open("commands.txt", "r")
-
-        #Starting from the beginning of the file
-        selected_cmd_file.seek(0)
-
-        #Writing each line in the file to the device
-        for each_line in selected_cmd_file.readlines():
-            connection.write(each_line + '\n')
-            time.sleep(2)
-
-        connection.write("end\n")
-        connection.write("write memory\n")
-        connection.write("exit\n")
-        time.sleep(2)
-
-        #Closing the file
-        selected_cmd_file.close()
-
-        #Write output to log file
-        # str_all = connection.read_all()
-        # log = open("log_out.txt", 'a')
-        # log.write(str_all)
-
-
-        #Test for reading command output
-     #   switch_output = connection.read_very_eager()
-     #   print switch_output
-
-        #Closing the connection and logging success
-        connection.close()
-        print(ip + " passed")
-
-
-    #Logging Exception Failure
-    except IOError:
-        print(ip + " failed")
-
+        for items in host_output:
+            print(items)
 
 if __name__=="__main__":
     try:
